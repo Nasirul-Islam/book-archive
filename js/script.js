a10dd0802912f5c525e2046306370c42e1d11d24
Nasirul-Islam <nasirulislam866@gmail.com>
2021-09-02 08:10:42 +0600

    second commit

--- js/script.js
+++ js/script.js
@@ -3,17 +3,45 @@ const loadBooks = () => {
     const inputField = document.getElementById('inputField');
     const inputValue = inputField.value;
     inputField.value = '';
+    const emptyString = document.getElementById('emptyString')
     // console.log(inputValue);
-    fetch(`http://openlibrary.org/search.json?q=${inputValue}`)
-        .then(res => res.json())
-        .then(data => displayBooks(data.docs))
+    if (inputValue === '') {
+        // console.log('hello')
+        emptyString.classList.remove('d-none')
+    }
+    // else if (inputValue === 'null') {
+    //     console.log('hello')
+    // }
+    else {
+        fetch(`http://openlibrary.org/search.json?q=${inputValue}`)
+            .then(res => res.json())
+            .then(data => displayBooks(data.docs))
+        emptyString.classList.add('d-none')
+    }
 }
 
 const displayBooks = books => {
-    // console.log(books)
+    // console.log(books.length)
+    const resultCount = document.getElementById('resultCount')
+    resultCount.textContent = '';
+    // If the search results do not match
+    if (books.length === 0) {
+        const div = document.createElement('div')
+        div.innerHTML = `
+            <h2 class="text-primary">Search doesn't match</h2>
+        `;
+        resultCount.appendChild(div)
+    } else {
+        const div = document.createElement('div')
+        div.innerHTML = `
+            <h2 class="text-primary">Search result found ${books.length}</h2>
+        `;
+        resultCount.appendChild(div)
+    }
+    // showing books  after search result
     const showBooks = document.getElementById('showBooks')
-    // showBooks.textContent='';
-    books.forEach(book => {
+    showBooks.textContent = '';
+    books?.forEach(book => {
         console.log(book)
         const div = document.createElement('div')
         div.classList.add('col')
@@ -22,7 +50,7 @@ const displayBooks = books => {
                     <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h4 class="card-title">Name: ${book.title}</h4>
-                    <h5>Writer: ${book.author_name[0]}</h5>
+                    <h5>Author: ${book.author_name[0]}</h5>
                     <h5>First publish: ${book.first_publish_year}</h5>
                     <h5>Publisher: ${book.publisher[0]}</h5>
                 </div>
@@ -32,24 +60,3 @@ const displayBooks = books => {
     })
 }
 
-/*
-.title
-.author_name[0]
-.first_publish_year
-.publisher[0]
-<p class="card-text"></p>
-.author_name[0]
-.first_publish_year
-
-.cover_i
- https://covers.openlibrary.org/b/id/{cover_i}-M.jpg
-
-https://covers.openlibrary.org/b/id/{cover_i}-L.jpg
-
-
-
-
-
-
-
-*/
