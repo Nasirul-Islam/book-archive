a555d64d56c861437ca03f1eccedf3128834ac5a
Nasirul-Islam <nasirulislam866@gmail.com>
2021-09-02 15:58:56 +0600

    organised code

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// load API
+// load API after click
 const loadBooks = () => {
     const inputField = document.getElementById('inputField');
     const defaultMes = document.getElementById('defaultMes')
@@ -19,7 +19,7 @@ const loadBooks = () => {
 
     }
 }
-
+// display Books after search
 const displayBooks = books => {
     const resultCount = document.getElementById('resultCount')
     const showBooks = document.getElementById('showBooks')
@@ -27,30 +27,29 @@ const displayBooks = books => {
     showBooks.textContent = '';
     resultCount.classList.remove('d-none')
     showBooks.classList.remove('d-none')
-    // If the search results do not match
+    // If the search results match or do not match
     if (books.length === 0) {
         const div = document.createElement('div')
         div.innerHTML = `
-            <h2 class="text-primary">Search doesn't match</h2>
+            <h2 class="text-primary text-end">no result found</h2>
         `;
         resultCount.appendChild(div)
     } else {
         const div = document.createElement('div')
         div.innerHTML = `
-            <h2 class="text-primary">Search result found ${books.length}</h2>
+            <h2 class="text-primary text-end">Search result found ${books.length}</h2>
         `;
         resultCount.appendChild(div)
     }
-    // showing books  after search result
+    // showing books after search result
     books?.forEach(book => {
-        console.log(book)
         const div = document.createElement('div')
         div.classList.add('col')
         div.innerHTML = `
             <div class="card h-100">
                     <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h4 class="card-title">Name: ${book.title}</h4>
+                    <h4 class="card-title fw-bold">Name: ${book.title}</h4>
                     <h5>Author: ${book.author_name[0] ? book.author_name[0] : ''}</h5>
                     <h5>First publish: ${book.first_publish_year}</h5>
                     <h5>Publisher: ${book.publisher[0] ? book.publisher[0] : ''}</h5>
